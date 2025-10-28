0256db7cfee942ab8802768897999994af69c22e
Süleyman GÜNEL <39226397+suleymangunel@users.noreply.github.com>
2025-10-28 22:25:16 +0300

    Add files via upload

---
+++ main/fonts/ink_free_12.h
@@ -0,0 +1,16 @@
+#ifndef INK_FREE_12_H
+#define INK_FREE_12_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+
+extern const lv_font_t ink_free_12;
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*INK_FREE_12_H*/
